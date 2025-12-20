cfb98258d626d875dc594f937778a6c3a90020dd
COLDH <3164886376@qq.com>
2025-12-20 13:11:34 +0800

    week1:2025210858 个人任务提交

---
+++ code/2025210858/test_c_program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argn,const char* argv[]){
+	printf("-----------------------------------------\n");
+	printf("A test C program\n");
+	printf("-----------------------------------------\n");
+	return 0;
+}
